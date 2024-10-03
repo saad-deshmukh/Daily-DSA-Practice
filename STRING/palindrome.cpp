474c548708ad9cdd5c210fb8c30de2380f85737a
saad <deshmukhsaad13@gmail.com>
2024-10-03 21:46:12 +0530

    checking palindrom

---
+++ STRING/palindrome.cpp
@@ -0,0 +1,36 @@
+/*   Given a string S consisting of N characters of the English alphabet, 
+the task is to check if the given string is a palindrome. 
+If the given string is a palindrome, then print “Yes“. Otherwise, print “No“.*/ 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string isPalindrome(string S)
+{
+	
+	string P = S;
+
+	// Reverse the string P
+	reverse(P.begin(), P.end());
+
+	// If S is equal to P
+	if (S == P) {
+		// Return "Yes"
+		return "Yes";
+	}
+	// Otherwise
+	else {
+		// return "No"
+		return "No";
+	}
+}
+
+// Driver Code
+int main()
+{
+	string S = "ABCDCBA";
+	cout << isPalindrome(S);
+
+	return 0;
+}
