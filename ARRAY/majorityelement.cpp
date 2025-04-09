794b739ab498cdc5509c858e06e153f1a8cf786a
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-04-09 11:17:44 +0530

    Added another appraoch for this code

--- ARRAY/majorityelement.cpp
+++ ARRAY/majorityelement.cpp
@@ -13,3 +13,49 @@ int main(){
     vector<int> arrb={4,5,4,6,8,4,1,4,3,4,2,6};
     cout << "the majority element is " << majorityelement(arrb);
 }
+
+/*
+  SECOND APPROACH 
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std ;
+
+int majority(int arr[],int n ){
+    
+    int majority = arr[0];
+    int count =  1 ; 
+    
+    for(int i = 1 ; i < n ; i++ ){
+        
+        
+        if (count  == 0 )  {
+            
+            majority= arr[i];
+            count ++ ; 
+        }
+        else if  (arr[i] == majority){
+            
+            count ++ ; 
+        }
+        else  {
+            count -- ; 
+        }
+        
+    }
+    return majority ;
+    
+}
+
+int main() {
+    int arr[] = {2,2,1,1,1,2,2};
+    int  j = 7 ;
+
+int  ans = majority(arr,j);
+    
+cout << "majority element from the given array is: " <<ans ; 
+return 0 ;
+}
+
+
+
+*/
