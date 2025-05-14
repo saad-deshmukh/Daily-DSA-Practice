e395b4db877206bdf210d767c32a2cf397eabd1a
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-05-14 16:29:13 +0530

    missing number from given range

---
+++ ARRAY/leetcode268.cpp
@@ -0,0 +1,53 @@
+/*Given an array nums containing n distinct numbers in the range [0, n],
+ return the only number in the range that is missing from the array.
+ Example 1:
+
+Input: nums = [3,0,1]
+
+Output: 2
+
+Explanation:
+
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+
+Input: nums = [0,1]
+
+Output: 2
+
+Explanation:
+
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+
+Output: 8
+
+Explanation:
+
+n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ */
+#include<bits/stdc++.h>
+using namespace std ; 
+  int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        double  totalsum= n * (n+1) / 2 ;
+
+        double givensum = 0;
+
+        for(int i = 0 ; i<nums.size() ; i++){
+
+            givensum += nums[i];
+        }
+        double ans =  totalsum-givensum;
+        return ans ; 
+    }
+
+    int main(){
+
+        vector<int>nums = {9,6,4,2,3,5,7,0,1};
+       cout <<  missingNumber(nums);
+    }
