6cf400a9eb2c68d9ce6c5e211c7e5954db9c3458
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-08-06 10:22:12 +0530

    another way to solve it

--- GENERAL QUESTIONS/leetcode13.cpp
+++ GENERAL QUESTIONS/leetcode13.cpp
@@ -1,32 +1,65 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int romantoint(string s){
+   
+//   unordered_map<char, int> roman_to_int = {
+//         {'I', 1}, {'V', 5}, {'X', 10},
+//         {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+//     };
+//     int total = 0 ;
+//     int prev_value = 0 ;
+//      for (int i = s.size() - 1; i >= 0; --i) {
+//         int current_value = roman_to_int[s[i]];
+//         if (current_value < prev_value) {
+//             total -= current_value; // Subtract if smaller
+//         } else {
+//             total += current_value; // Add if larger or equal
+//         }
+
+//         // Update the previous value
+//         prev_value = current_value;
+//     }
+
+//     return total;
+// }
+
+// int main(){
+
+//     string s = "XXIXI";
+//     cout << romantoint(s);
+
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int romantoint(string s){
-   
-  unordered_map<char, int> roman_to_int = {
-        {'I', 1}, {'V', 5}, {'X', 10},
-        {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-    };
-    int total = 0 ;
-    int prev_value = 0 ;
-     for (int i = s.size() - 1; i >= 0; --i) {
-        int current_value = roman_to_int[s[i]];
-        if (current_value < prev_value) {
-            total -= current_value; // Subtract if smaller
-        } else {
-            total += current_value; // Add if larger or equal
+    unordered_map<char,int>mpp;
+        mpp['I'] = 1;
+        mpp['V'] = 5;
+        mpp['X'] = 10;
+        mpp['L'] = 50;
+        mpp['C'] = 100;
+        mpp['D'] = 500;
+        mpp['M'] = 1000;
+
+        int n = s.length();
+        int ans = mpp[s[n-1]];
+        for(int i=n-2;i>=0;i--){
+
+            if(mpp[s[i]] < mpp[s[i+1]]){
+                ans -= mpp[s[i]];
+            }
+            else{
+                ans += mpp[s[i]];
+            }
         }
-
-        // Update the previous value
-        prev_value = current_value;
-    }
-
-    return total;
+        return ans ; 
+    
 }
-
 int main(){
-
-    string s = "XXIXI";
-    cout << romantoint(s);
-
+   string s = "MCMXCIV";
+   int ans = romantoint(s);
+   cout << "the roman numberr for " << s << " is: " << ans ; 
 }
