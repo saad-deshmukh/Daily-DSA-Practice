d8ce25a2958c4c7131b1f7b4ada985ab2d1be9df
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-08-18 22:05:28 +0530

    three sum problem

---
+++ ARRAY/leetcode15.cpp
@@ -0,0 +1,88 @@
+/*
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threesum(vector<int> &nums)
+{
+
+    vector<vector<int>> ans;
+    int n = nums.size() - 1;
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        int j = i + 1;
+        int k = n;
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+
+            if (sum < 0)
+            {
+                j++;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+                while (k > j && nums[k] == nums[k + 1])
+                    k--;
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> vec = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> answer = threesum(vec);
+
+    for (int i = 0; i < answer.size(); i++)
+    {
+        for (int j = 0; j < answer[i].size(); j++)
+        {
+            cout << answer[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
