41d6e2dd09d9f8558214868429c115aa339cde1d
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-09-06 16:45:00 +0530

    Substring Matching Pattern

--- ARRAY/leetcode3407.cpp
+++ ARRAY/leetcode3407.cpp
@@ -1,40 +1,21 @@
 /*
 You are given a string s and a pattern string p, where p contains exactly one '*' character.
-
 The '*' in p can be replaced with any sequence of zero or more characters.
-
 Return true if p can be made a substring of s, and false otherwise.
-
-
-
 Example 1:
-
 Input: s = "leetcode", p = "ee*e"
-
 Output: true
-
 Explanation:
-
 By replacing the '*' with "tcod", the substring "eetcode" matches the pattern.
-
 Example 2:
-
 Input: s = "car", p = "c*v"
-
 Output: false
-
 Explanation:
-
 There is no substring matching the pattern.
-
 Example 3:
-
 Input: s = "luck", p = "u*"
-
 Output: true
-
 Explanation:
-
 The substrings "u", "uc", and "uck" match the pattern.
 */
 #include <bits/stdc++.h>
