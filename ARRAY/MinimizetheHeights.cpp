2a11389c0db8b063fa6cd5ee66bb56a64f01fa5f
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-09-12 13:11:31 +0530

    MInimum Possible difference between the highest and smallest Tower after, either Adding or Subtracting K  (Greedy  Solution)

---
+++ ARRAY/MinimizetheHeights.cpp
@@ -0,0 +1,58 @@
+/*Given an array arr[] denoting heights of n towers and a positive integer k.
+For each tower, you must perform exactly one of the following operations exactly once.
+Increase the height of the tower by k
+Decrease the height of the tower by k
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+You can find a slight modification of the problem here.
+Note: It is compulsory to increase or decrease the height by k for each tower. After the operation, the resultant array should not contain any negative integers.
+Examples :
+Input: k = 2, arr[] = [1, 5, 8, 10]
+Output: 5
+Explanation: The array can be modified as [1+k, 5-k, 8-k, 10-k] = [3, 3, 6, 8]. The difference between the largest and the smallest is 8-3 = 5.
+Input: k = 3, arr[] = [3, 9, 12, 16, 20]
+Output: 11
+Explanation: The array can be modified as [3+k, 9+k, 12-k, 16-k, 20-k] = [6, 12, 9, 13, 17]. The difference between the largest and the smallest is 17-6 = 11. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimizeHeight(vector<int> arr, int k)
+    {
+        int n = arr.size();
+        if (n == 1)
+            return 0;
+
+        sort(arr.begin(), arr.end());
+        int result = arr[n - 1] - arr[0];
+
+        int smallest = arr[0] + k;
+        int largest = arr[n - 1] - k;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minHeight = min(smallest, arr[i - 1] - k);
+            int maxHeight = max(largest, arr[i] + k);
+
+            if (minHeight < 0)
+            {
+                continue;
+            }
+            result = min(result, maxHeight - minHeight);
+        }
+        return result;
+    }
+};
+
+int main()
+{
+
+    Solution s;
+    vector<int> arr = {3, 9, 12, 16, 20};
+    int k = 3;
+    int ans = s.minimizeHeight(arr, k);
+    cout << "Minimized Height is: " << ans << endl;
+    return 0;
+}
