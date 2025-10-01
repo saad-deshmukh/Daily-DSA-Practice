d46fac8f4465faadeb13323cfbdf606af74ae784
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-10-01 10:56:54 +0530

    all possible permutaions using in built (next_permutaion) function

---
+++ ARRAY/leetcode46.cpp
@@ -0,0 +1,45 @@
+/*Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+Input: nums = [1]
+Output: [[1]*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        do
+        {
+            ans.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> answer = s.permute(nums);
+    for (const auto &row : answer)
+    {
+        for (int val : row)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
