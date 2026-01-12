9696ed408914ebf9f56a1b56fc92bbf8423e1e67
saad-deshmukh <deshmukhsaad13@gmail.com>
2026-01-12 19:26:30 +0530

    Implement greedy solution to calculate minimum time to visit points

---
+++ ARRAY/leetcode1266.cpp
@@ -0,0 +1,32 @@
+/*On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi].
+Return the minimum time in seconds to visit all the points in the order given by points.
+You can move according to these rules:
+In 1 second, you can either:
+move vertically by one unit,
+move horizontally by one unit, or
+move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+You have to visit the points in the same order as they appear in the array.
+You are allowed to pass through points that appear later in the order, but these do not count as visits.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0 ; int n = points.size();
+        for(int i=1;i<n;i++){
+            ans += max(abs(points[i][0]-points[i-1][0]),
+                       abs(points[i][1]-points[i-1][1]) );
+        }
+        return ans;
+
+    }
+};
+
+int main(){
+    Solution s;
+    vector<vector<int>> points= {{1,1},{3,4},{-1,0}};
+    int answer =  s.minTimeToVisitAllPoints(points);
+    cout << "Minimum Time to visiting all the points is: " << answer;
+    return 0 ; 
+}
